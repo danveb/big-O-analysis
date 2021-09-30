492c1c9b865625d41b7914e78aaebf7f97afcba6
Daniel Bae <danibae91@icloud.com>
2021-09-30 09:52:22 -0700

    submit answers to part 3

---
+++ 3-short-answer/index.js
@@ -0,0 +1,35 @@
+// 1. n^2 + n is O(n^2)
+// - True; n^2 is a greater expression regardless of input "n"
+
+// 2. n^2 * n is O(n^3) 
+// - True; n * n * n = n^3 
+
+// 3. n^2 + n is O(n) 
+// - False; O(n^2) because arithmetic operations don't matter
+
+// 4. .indexOf array? 
+// - O(n) 
+
+// 5. .includes array?
+// - O(n) 
+
+// 6. .forEach array? 
+// - O(n)
+
+// 7. .sort array? 
+// - O(n log n) 
+
+// 8. .unshift array? 
+// - O(n)
+
+// 9. .push array?
+// - O(1)
+
+// 10. .splice array? 
+// - O(n)
+
+// 11. .pop array? 
+// - O(1)
+
+// 12. Object.keys()?
+// - O(n) 
